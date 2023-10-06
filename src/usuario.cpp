e379a3827944af8f39a800dede89c844cc930f72
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-10-06 09:46:26 -0300

    fix: Usuario

--- src/usuario.cpp
+++ src/usuario.cpp
@@ -1,43 +1,42 @@
 #include "CarRental.hpp"
-using namespace std;
-class Usuario{
-    protected:
-        string cpf;
-        string nome;
-        string endereco;
-        string telefone;
-    
-    public:
-        Usuario();
-        Usuario(string cpf, string nome, string endereço, string telefone){
-            this->cpf = cpf;
-            this->nome = nome;
-            this->endereco = endereco;
-            this->telefone = telefone;
-        };
-        string getCpf(){
-            return this->cpf;
-        };
-        string getNome(){
-            return this->nome;
-        };
-        string getEndereco(){
-            return this->endereco;
-        };
-        string getTelefone(){
-            return this->telefone;
-        };
-        void setCpf(string){
-            this->cpf = cpf;
-        };
-        void setNome(string){
-            this->nome = nome;
-        };
-        void setEndereco(string){
-            this->endereco = endereco;
-        };
-        void setTelefone(string){
-            this->telefone = telefone;
-        };
-        
+
+Usuario::Usuario(){};
+
+Usuario::Usuario(string cpf, string nome, string endereço, string telefone){
+    this->cpf = cpf;
+    this->nome = nome;
+    this->endereco = endereco;
+    this->telefone = telefone;
+};
+
+string Usuario::getCpf(){
+    return this->cpf;
+};
+
+string Usuario::getNome(){
+    return this->nome;
+};
+
+string Usuario::getEndereco(){
+    return this->endereco;
+};
+
+string Usuario::getTelefone(){
+    return this->telefone;
+};
+
+void Usuario::setCpf(string){
+    this->cpf = cpf;
+};
+
+void Usuario::setNome(string){
+    this->nome = nome;
+};
+
+void Usuario::setEndereco(string){
+    this->endereco = endereco;
+};
+
+void Usuario::setTelefone(string){
+    this->telefone = telefone;
 };
